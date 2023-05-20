588ca07b2af17ec742de3b920f30ba23502f96bd
canozdemir <canozdemir92@gmail.com>
2023-05-20 18:09:28 +0300

    Add project files.

---
+++ Pub.Sub.Publisher/Program.cs
@@ -0,0 +1,12 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer connection = await ConnectionMultiplexer.ConnectAsync("localhost:1453");
+
+ISubscriber redis = connection.GetSubscriber();
+
+while(true)
+{
+    Console.WriteLine("Message : ");
+    string message = Console.ReadLine();
+    await redis.PublishAsync("myChannel",message);
+}
